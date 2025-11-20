adb2ea071ad7b61a1cd37f295713b75442f50064
bhag773 <gs05268348@priestley.ac.uk>
2025-11-20 13:50:06 +0000

    Add files via upload

--- bubble sort algorith.py
+++ bubble sort algorith.py
@@ -1,12 +1,18 @@
+
 items = [23, 41, 11, 17 , 34, 56]
 n = len(items)-1
-index = 0
-for index in range(0,n):
+
+swapped = True
+
+while swapped:
+    swapped = False
     for index in range(0,n):
+       
         if (items[index] > items[index+1]):
             temp = items[index]
             items[index] = items[index+1]
             items[index+1] = temp
+            swapped = True
 
 print(items)
 print(f"Length of the list {n}")
